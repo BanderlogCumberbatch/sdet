c26896788aed85fb357581db7e5e98a176cecd6f
BanderlogCumberbatch <kutlub.artur@gmail.com>
2025-10-06 18:48:25 +0400

    grid added

---
+++ src/test/java/grid/BaseTest.java
@@ -0,0 +1,33 @@
+package grid;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws MalformedURLException {
+        System.out.println("Starting test in thread: " + Thread.currentThread().getId());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setPlatform(Platform.WIN11);
+        capabilities.setBrowserName("chrome");
+
+        driver = new RemoteWebDriver(new URL("http://192.168.31.145:4444/"), capabilities);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
--- src/test/java/grid/start-grid.bat
+++ src/test/java/grid/start-grid.bat
@@ -2,18 +2,13 @@
 echo Starting Selenium Grid...
 
 :: Запуск Hub в новом окне
-start "Selenium Hub" cmd /k "java -jar selenium-server-4.35.0.jar -role hub -port 4444"
-timeout /t 3
+start "Selenium Hub" cmd /k "java -jar selenium-server-4.35.0.jar hub"
 
 :: Запуск Node 1 в новом окне
-start "Selenium Node 1" cmd /k "java -jar selenium-server-4.35.0.jar -role node -hub http://localhost:4444/grid/register -port 5555 -maxSession 5"
+start "Selenium Node 1" cmd /k "java -jar selenium-server-4.35.0.jar node --port 5555 --max-threads 2"
 
 :: Запуск Node 2 в новом окне
-start "Selenium Node 2" cmd /k "java -jar selenium-server-4.35.0.jar -role node -hub http://localhost:4444/grid/register -port 5556 -maxSession 5"
+start "Selenium Node 2" cmd /k "java -jar selenium-server-4.35.0.jar node --port 6666 --max-threads 2"
 
-echo Selenium Grid is running...
-echo Hub: http://localhost:4444
-echo Nodes: 5555, 5556
-echo.
-echo Grid started successfully! Close command windows to stop.
-pause
+
+echo Grid started successfully
